4779a2a7b574f515053ce0a81d980304dc2abae5
Cole Brokamp <cole.brokamp@gmail.com>
2021-01-20 11:08:44 -0500

    save nearby PM25 for later usage in prediction

--- 02_get_AQS_data.R
+++ 02_get_AQS_data.R
@@ -28,30 +28,28 @@ d <- bind_rows(d)
 
 d <- d %>%
   group_by(id, lat, lon, date) %>%
-  summarize(pm25 = mean(pm25))
+  summarize(pm25 = mean(pm25)) %>%
+  ungroup()
 
 d <- sf::st_as_sf(d, coords = c("lon", "lat"), crs = 4326)
+d <- sf::st_as_sf(d)
 
 # remove sites outside of contig US by intersecting in epsg 5072
 options(tigris_use_cache = TRUE)
 options(tigris_class = "sf")
+
 us <-
   tigris::states() %>%
   filter(! NAME %in% c('United States Virgin Islands',
                        'Guam', 'Commonwealth of the Northern Mariana Islands',
                        'American Samoa', 'Puerto Rico',
                        'Alaska', 'Hawaii')) %>%
-  st_union()
+  st_union() %>%
+  st_transform(5072)
 
-d$intersection <- st_intersects(
-  st_transform(d, 5072),
-  st_transform(us, 5072),
-  sparse = FALSE
-)
+d_intersection <- st_intersects(st_transform(d, 5072), us, sparse = FALSE)
 
-d <- d %>%
-  filter(intersection) %>%
-  select(-intersection)
+d <- filter(d, d_intersection)
 
 # geohash
 d$h3 <- h3::geo_to_h3(d, res = 8)
@@ -61,14 +59,17 @@ d_nearby_pm <-
   d %>%
   mutate(h3_5 = h3::h3_to_parent(h3, res = 5)) %>%
   group_by(date, h3_5) %>%
-  summarize(median_pm25 = median(pm25, na.rm = TRUE)) %>%
+  summarize(mean_pm25 = mean(pm25, na.rm = TRUE), .groups = "keep") %>%
   mutate(
-    median_pm25_ytd = dplyr::lag(median_pm25, n = 1, order_by = date),
-    median_pm25_tmw = dplyr::lead(median_pm25, n = 1, order_by = date)
+    mean_pm25_ytd = dplyr::lag(mean_pm25, n = 1, order_by = date),
+    mean_pm25_tmw = dplyr::lead(mean_pm25, n = 1, order_by = date)
   ) %>%
   group_by(date, h3_5) %>%
-  mutate(nearby_pm25 = median(c(median_pm25, median_pm25_ytd, median_pm25_tmw), na.rm = TRUE)) %>%
+  mutate(nearby_pm25 = mean(c(mean_pm25, mean_pm25_ytd, mean_pm25_tmw), na.rm = TRUE)) %>%
   select(h3_5, date, nearby_pm25)
+
+saveRDS(d_nearby_pm, "d_nearby_pm.rds")
+system("aws s3 cp d_nearby_pm.rds s3://geomarker/st_pm_hex/d_nearby_pm.rds")
 
 d$h3_5 <- h3::h3_to_parent(d$h3, res = 5)
 d <- left_join(d, st_drop_geometry(d_nearby_pm), by = c("h3_5", "date"))
