ef8422fa77b8d7cc4a05d5d8f7a12e8ec66e7ac1
Cole Brokamp <cole.brokamp@gmail.com>
2021-01-29 09:29:17 -0500

    finalize training model

--- 11_train_pred_model.R
+++ 11_train_pred_model.R
@@ -13,49 +13,61 @@ library(grf)
 set.seed(224)
 
 # system("aws s3 cp s3://geomarker/st_pm_hex/h3data_train.fst .")
-d <- fst::read_fst("h3data_train.fst", as.data.table = TRUE)
-d$nei_year <- NULL
-d$dow <- as.numeric(d$dow)
-d$holiday <- as.numeric(d$holiday)
+d <-
+  fst::read_fst("h3data_train.fst", as.data.table = TRUE) %>%
+  select(-nei_year) %>%
+  mutate(dow = as.numeric(dow),
+         holiday = as.numeric(holiday))
 
 # round training data to 4 significant digits
 d$pm25 <- signif(d$pm25, digits = 4)
 
+## summarize number of clusters and  cluster sizes
+n_distinct(d$h3)
+
+d %>%
+  group_by(h3) %>%
+  summarize(n = n()) %>%
+  pull(n) %>%
+  summary()
+
 tictoc::tic()
-grf_cluster <-
+grf <-
     regression_forest(
-        X = as.data.frame(d) %>% select(-date, -h3, -pm25),
+        X = as.data.frame(d) %>% select(-date, -h3, -pm25, -county_fips),
         Y = d$pm25,
         seed = 224,
         num.threads = parallel::detectCores(),
-        compute.oob.predictions = FALSE,
+        compute.oob.predictions = TRUE,
         sample.fraction = 0.5,
-        num.trees = 1000,
-        mtry = 26,
-        min.node.size = 5,
+        num.trees = 500,
+        mtry = 27,
+        min.node.size = 1,
         alpha = 0.05,
-        honesty = TRUE, honesty.fraction = 0.5, honesty.prune.leaves = TRUE,
-        tune.parameters = "none",
+        imbalance.penalty = 0,
+        honesty = FALSE,
         clusters = as.factor(d$h3), # 1,912 possible h3 'levels'
-        equalize.cluster.weights = FALSE
+        equalize.cluster.weights = FALSE,
+        tune.parameters = "none"
     )
 tictoc::toc()
 
-qs::qsave(grf_cluster, "st_pm_hex_grf_cluster.qs", compress = 22, nthreads = parallel::detectCores())
-system("aws s3 cp st_pm_hex_grf_cluster.qs s3://geomarker/st_pm_hex/st_pm_hex_grf_cluster.qs")
-
-grf_cluster <- qs::qread("st_pm_hex_grf_cluster.qs", nthreads = parallel::detectCores())
-
-grf_cluster_preds_oob <- predict(grf_cluster, estimate.variance = TRUE)
+qs::qsave(grf, "st_pm_hex_grf.qs", compress = 22, nthreads = parallel::detectCores())
+system("aws s3 cp st_pm_hex_grf.qs s3://geomarker/st_pm_hex/st_pm_hex_grf.qs")
 
 tictoc::tic()
-grf_cluster_preds <- predict(grf_cluster, as.data.frame(d) %>% select(-date, -h3, -pm25), estimate.variance = TRUE)
+grf_preds_oob <- predict(grf, estimate.variance = TRUE)
 tictoc::toc()
 
-d$pm_pred_cluster <- grf_cluster_preds$predictions
-d$pm_pred_cluster_se <- sqrt(grf_cluster_preds$variance.estimates)
-d$pm_pred_cluster_oob <- grf_cluster_preds_oob$predictions
-d$pm_pred_cluster_oob_se <- sqrt(grf_cluster_preds_oob$variance.estimates)
+d$pm_pred_oob <- grf_preds_oob$predictions
+d$pm_pred_oob_se <- sqrt(grf_preds_oob$variance.estimates)
+
+tictoc::tic()
+grf_preds <- predict(grf, as.data.frame(d) %>% select(-date, -h3, -pm25, -county_fips, -pm_pred_oob, -pm_pred_oob_se), estimate.variance = TRUE)
+tictoc::toc()
+
+d$pm_pred <- grf_preds$predictions
+d$pm_pred_se <- sqrt(grf_preds$variance.estimates)
 
 # round all predictions and SEs to 4 significant digits
 d <- d %>%
