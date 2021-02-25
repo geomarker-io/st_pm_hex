eb1cf7e3e0676a16fcc7c482e08be7aae51cba41
Cole Brokamp <cole.brokamp@gmail.com>
2021-02-25 15:57:18 -0500

    yes; all models will be by year now

--- 11_train_pred_model.R
+++ 11_train_pred_model.R
@@ -1,8 +1,3 @@
-# using CCHMC cluster for this training:
-# bsub -Is -M 200000 -n 24 -W 24:00 -R "span[ptile=24]" "module load singularity; ~/singr_4.0.sif"
-
-# https://linuxhandbook.com/increase-swap-ubuntu/
-
 library(data.table)
 library(tidyr)
 library(dplyr)
@@ -12,80 +7,131 @@ library(grf)
 
 set.seed(224)
 
+pred_names <- c(
+  "nearby_pm25",
+  "hpbl",
+  "doy",
+  "x",
+  "air.2m",
+  "vwnd.10m",
+  "y",
+  "pres.sfc",
+  "vis",
+  "rhum.2m",
+  "nei_event",
+  "nei_nonpoint",
+  "prate",
+  "nei_dist",
+  "uwnd.10m",
+  "nei_point",
+  "nei_nonroad",
+  "green",
+  "nei_onroad"
+)
+
 # system("aws s3 cp s3://geomarker/st_pm_hex/h3data_train.fst .")
 d <-
   fst::read_fst("h3data_train.fst", as.data.table = TRUE) %>%
-  select(-nei_year) %>%
-  mutate(dow = as.numeric(dow),
-         holiday = as.numeric(holiday))
+  select(all_of(c(pred_names, "pm25", "h3", "year", "date")))
 
 # round training data to 4 significant digits
 d$pm25 <- signif(d$pm25, digits = 4)
 
-## summarize number of clusters and  cluster sizes
-n_distinct(d$h3)
+## summarize number of clusters and cluster sizes
+n_distinct(d$h3) # 1,684
+
+n_distinct(select(d, h3, year)) #17,813
 
 d %>%
-  group_by(h3) %>%
+  group_by(h3, year) %>%
   summarize(n = n()) %>%
   pull(n) %>%
   summary()
 
-## TODO filter to only clusters with at least n observations; use equalize.cluster.weights
+# 100 is about equal to the 1st quartile of cluster sizes (n = 97)
 
+d %>%
+  group_by(h3, year) %>%
+  summarize(n = n()) %>%
+  group_by(n >= 100) %>%
+  summarize(n = n())
 
-## TODO how can we plot OOB error rate with number of trees
+# keeps 13,221 of 17,813 possible clusters
 
-## consider using lots of smaller forests and then unioning these
+d <-
+  d %>%
+  group_by(year, h3) %>%
+  nest() %>%
+  mutate(n = purrr::map_dbl(data, nrow)) %>%
+  filter(n > 100) %>%
+  select(-n) %>%
+  unnest(cols = c(data)) %>%
+  group_by(year) %>%
+  nest()
 
-tictoc::tic()
-grf <-
+train_and_save_grf <- function(year) {
+
+  d_train <-
+    d %>%
+    filter(year == year) %>%
+    pull(data) %>%
+    .[[1]]
+
+  grf <-
     regression_forest(
-        X = as.data.frame(d) %>% select(-date, -h3, -pm25, -county_fips),
-        Y = d$pm25,
-        seed = 224,
-        num.threads = parallel::detectCores(),
-        compute.oob.predictions = TRUE,
-        sample.fraction = 0.5,
-        num.trees = 500, # default 2000
-        mtry = 27,
-        min.node.size = 1, # default 5
-        alpha = 0.05,
-        imbalance.penalty = 0,
-        honesty = FALSE,
-        clusters = as.factor(d$h3), # 1,912 possible h3 'levels'
-        equalize.cluster.weights = FALSE,
-        tune.parameters = "none"
+      X = select(d_train, all_of(pred_names)),
+      Y = d_train$pm25,
+      seed = 224,
+      num.threads = parallel::detectCores(),
+      compute.oob.predictions = TRUE,
+      sample.fraction = 0.5,
+      num.trees = 2000, # default 2000
+      mtry = 19, # 19 total predictors now
+      min.node.size = 1, # default 5
+      alpha = 0.05,
+      imbalance.penalty = 0,
+      honesty = FALSE,
+      clusters = as.factor(d_train$h3), # 1,912 possible h3 'levels'
+      equalize.cluster.weights = FALSE,
+      tune.parameters = "none"
     )
-tictoc::toc()
 
-qs::qsave(grf, "st_pm_hex_grf.qs", nthreads = parallel::detectCores())
-system("aws s3 cp st_pm_hex_grf.qs s3://geomarker/st_pm_hex/st_pm_hex_grf.qs")
+  qs::qsave(grf,
+    glue::glue("st_pm_hex_grf_{year}.qs"),
+    nthreads = parallel::detectCores()
+  )
 
-tictoc::tic()
-grf_preds_oob <- predict(grf, estimate.variance = TRUE)
-tictoc::toc()
+  system(glue::glue(
+    "aws s3 cp ",
+    "st_pm_hex_grf_{year}.qs ",
+    "s3://geomarker/st_pm_hex/st_pm_hex_grf_{year}.qs"
+  ))
 
-d$pm_pred_oob <- grf_preds_oob$predictions
-d$pm_pred_oob_se <- sqrt(grf_preds_oob$variance.estimates)
+  grf_preds_oob <- predict(grf, estimate.variance = TRUE)
 
-tictoc::tic()
-grf_preds <- predict(grf, as.data.frame(d) %>% select(-date, -h3, -pm25, -county_fips, -pm_pred_oob, -pm_pred_oob_se), estimate.variance = TRUE)
-tictoc::toc()
+  d_out <- select(d_train, h3, date)
 
-d$pm_pred <- grf_preds$predictions
-d$pm_pred_se <- sqrt(grf_preds$variance.estimates)
+  d_out$pm_pred_oob <- grf_preds_oob$predictions
+  d_out$pm_pred_oob_se <- sqrt(grf_preds_oob$variance.estimates)
+
+  grf_preds <- predict(grf, select(d_train, all_of(pred_names)), estimate.variance = TRUE)
+
+  d_out$pm_pred <- grf_preds$predictions
+  d_out$pm_pred_se <- sqrt(grf_preds$variance.estimates)
 
 # round all predictions and SEs to 4 significant digits
-d <- d %>%
+  d_out <-
+    d_out %>%
     mutate_at(vars(starts_with("pm_pred_")), signif, digits = 4)
 
-# save it
-qs::qsave(d, "h3_data_grf_preds.qs", nthreads = parallel::detectCores())
-system("aws s3 cp h3_data_grf_preds.qs s3://geomarker/st_pm_hex/h3_data_grf_preds.qs")
+  qs::qsave(d_out,
+    glue::glue("st_pm_hex_grf_preds_{year}.qs"),
+    nthreads = parallel::detectCores()
+  )
+}
 
-## query variable importance; can we reduce forest size?
-## less variables?, fewer trees?, smaller sample.fraction?
+## train_and_save_grf(2015)
 
-# a weighted sum of how many times a variable was used in splitting at each depth in the forest
-var_imp <- variable_importance(grf, decay.exponent = 2, max.depth = 4)
+purrr::walk(2000:2020, train_and_save_grf)
+
+## collect all predictions, combine, upload
