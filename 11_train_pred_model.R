025e9e163e3f8bf0b64c97d4d35b3ac756db548f
Cole Brokamp <cole.brokamp@gmail.com>
2021-03-01 20:25:46 -0500

    fix bug with iterating over year variable

--- 11_train_pred_model.R
+++ 11_train_pred_model.R
@@ -69,11 +69,11 @@ d <-
   group_by(year) %>%
   nest()
 
-train_and_save_grf <- function(year) {
+train_and_save_grf <- function(yr) {
 
   d_train <-
     d %>%
-    filter(year == year) %>%
+    filter(year == yr) %>%
     pull(data) %>%
     .[[1]]
 
@@ -97,14 +97,14 @@ train_and_save_grf <- function(year) {
     )
 
   qs::qsave(grf,
-    glue::glue("st_pm_hex_grf_{year}.qs"),
+    glue::glue("st_pm_hex_grf_{yr}.qs"),
     nthreads = parallel::detectCores()
   )
 
   system(glue::glue(
     "aws s3 cp ",
-    "st_pm_hex_grf_{year}.qs ",
-    "s3://geomarker/st_pm_hex/st_pm_hex_grf_{year}.qs"
+    "st_pm_hex_grf_{yr}.qs ",
+    "s3://geomarker/st_pm_hex/st_pm_hex_grf_{yr}.qs"
   ))
 
   grf_preds_oob <- predict(grf, estimate.variance = TRUE)
@@ -125,13 +125,20 @@ train_and_save_grf <- function(year) {
     mutate_at(vars(starts_with("pm_pred_")), signif, digits = 4)
 
   qs::qsave(d_out,
-    glue::glue("st_pm_hex_grf_preds_{year}.qs"),
+    glue::glue("st_pm_hex_grf_preds_{yr}.qs"),
     nthreads = parallel::detectCores()
   )
 }
 
-## train_and_save_grf(2015)
-
 purrr::walk(2000:2020, train_and_save_grf)
 
+
 ## collect all predictions, combine, upload
+
+purrr::map(2000:2020, ~ qs::qread(paste0("st_pm_hex_grf_preds_", ., ".qs"))) %>%
+  set_names(2000:2020) %>%
+  tibble::enframe(name = "year", value = "data") %>%
+  unnest(cols = c(data)) %>%
+  saveRDS("st_pm_hex_grf_preds.rds")
+
+system("aws s3 cp st_pm_hex_grf_preds.rds s3://geomarker/st_pm_hex/st_pm_hex_grf_preds.rds")
